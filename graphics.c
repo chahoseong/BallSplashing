00304944d862d84c8ea35daa4d15c05df5128e2b
chahoseong <hoseong@chahoseong.com>
2019-05-02 10:04:18 +0900

    Fixed Graphics Functions

--- graphics.c
+++ graphics.c
@@ -1,58 +1,60 @@
 #include "graphics.h"
 #include <Windows.h>
 
-static HANDLE frame_buffer[2];
+#define FRAME_BUFFER_COUNT 2
+
+/*
+Internal Variables
+*/
+static HANDLE frame_buffer[FRAME_BUFFER_COUNT];
 static int frame_cursor;
 
-static int width;
-static int height;
+static int frame_buffer_width;
+static int frame_buffer_height;
 
-int InitGraphics(int new_width, int new_height)
+/*
+Internal Function Declares
+*/
+HANDLE CreateFrameBuffer(int width, int height);
+
+/*
+External Function Definition
+*/
+int InitGraphics(int width, int height)
 {
-	COORD buffer_size = { width, height };
-	SMALL_RECT window_size;
-	window_size.Top = 0;
-	window_size.Left = 0;
-	window_size.Right = width - 1;
-	window_size.Bottom = height - 1;
+	for (int i = 0; i < FRAME_BUFFER_COUNT; ++i)
+	{
+		frame_buffer[i] = CreateBuffer(width, height);
+		if (!frame_buffer[i])
+			return 0;
+	}
 
-	// front buffer
-	frame_buffer[0] = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-	if (frame_buffer[0] == INVALID_HANDLE_VALUE)
-		return 0;
-	SetConsoleScreenBufferSize(frame_buffer[0], buffer_size);
-	SetConsoleWindowInfo(frame_buffer[0], TRUE, &window_size);
-
-	// back buffer
-	frame_buffer[1] = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-	if (frame_buffer[1] == INVALID_HANDLE_VALUE)
-		return 0;
-	SetConsoleScreenBufferSize(frame_buffer[1], buffer_size);
-	SetConsoleWindowInfo(frame_buffer[1], TRUE, &window_size);
-
-	width = new_width;
-	height = new_height;
+	frame_buffer_width = width;
+	frame_buffer_height = height;
 
 	return 1;
 }
 
 void CloseGraphics()
 {
-	CloseHandle(frame_buffer[0]);
-	CloseHandle(frame_buffer[1]);
+	for (int i = 0; i < FRAME_BUFFER_COUNT; ++i)
+	{
+		if (frame_buffer[i])
+			CloseHandle(frame_buffer[i]);
+	}
 }
 
 void ClearFrameBuffer()
 {
 	COORD start_position = { 0, 0 };
-	FillConsoleOutputCharacterA(frame_buffer, ' ', width * height, start_position, NULL);
+	FillConsoleOutputCharacterA(frame_buffer, ' ', frame_buffer_width * frame_buffer_height, start_position, NULL);
 }
 
 void WriteFrameBuffer(char* string, int size, int x, int y)
 {
 	COORD position = { x, y };
 	SetConsoleCursorPosition(frame_buffer[frame_cursor], position);
-	WriteFile(frame_buffer[frame_cursor], string, strlen(string), NULL, NULL);
+	WriteFile(frame_buffer[frame_cursor], string, size, NULL, NULL);
 }
 
 void SwapFrameBuffer()
@@ -61,7 +63,24 @@ void SwapFrameBuffer()
 	frame_cursor != frame_cursor;
 }
 
-HANDLE CurrentFrameBuffer()
+/*
+Internal Function Definition
+*/
+HANDLE CreateFrameBuffer(int width, int height)
 {
-	return frame_buffer[frame_cursor];
+	COORD buffer_size = { width, height };
+	SMALL_RECT window_size;
+	window_size.Top = 0;
+	window_size.Left = 0;
+	window_size.Right = width - 1;
+	window_size.Bottom = height - 1;
+
+	HANDLE frame_buffer = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	if (frame_buffer == INVALID_HANDLE_VALUE)
+		return NULL;
+
+	SetConsoleScreenBufferSize(frame_buffer, buffer_size);
+	SetConsoleWindowInfo(frame_buffer, TRUE, &window_size);
+
+	return frame_buffer;
 }
--- main.c
+++ main.c
@@ -2,9 +2,16 @@
 
 int main(void)
 {
-	const Timer* game_timer = CreateTimer();
-
+	Timer* game_timer = CreateTimer();
+	ResetTimer(game_timer);
 	
+	int is_running = 1;
+
+	while (is_running)
+	{
+		TickTimer(game_timer);
+	}
+
 
 	return 0;
 }
